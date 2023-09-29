f3ca55ecc84c545ecd9eaa545f384ef2c6309dfb
nabatwadenis <dnabatwa9@gmail.com>
2023-09-29 15:42:48 -0700

    google authentication first revision

--- picsgallery-frontend/src/App.js
+++ picsgallery-frontend/src/App.js
@@ -7,12 +7,13 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 const App = () => {
 
   return (
-    <GoogleOAuthProvider clientId={`${process.env.REACT_PUBLIC_GOOGLE_API_TOKEN}`}>
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_PUBLIC_GOOGLE_API_TOKEN}`}>
       <Routes>
         <Route path='login' element={<Login />}/>
         <Route path='/*' element={<Home />}/>
       </Routes>
     </GoogleOAuthProvider>
+    
   )
 }
 
--- picsgallery-frontend/src/components/Login.jsx
+++ picsgallery-frontend/src/components/Login.jsx
@@ -3,9 +3,27 @@ import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 import {FcGoogle} from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
-import logo from '../assets/logowhite.png'
+import logo from '../assets/logowhite.png';
+import { client } from '../client';
 
 const Login = () => {
+    const navigate = useNavigate();
+    const responseGoogle =(response) =>{
+        localStorage.setItem('user', JSON.stringify(response.clientId))
+        const {name, googleId, imageUrl} = response.clientId;
+        const doc ={
+            _id: googleId,
+            _type: 'user',
+            userName: name,
+            image: imageUrl,
+        };
+        client.createIfNotExists(doc)
+        .then(()=>{
+            navigate('/', { replace: true})
+
+        })
+
+    }
     const user = false;
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
@@ -24,15 +42,20 @@ const Login = () => {
         </div>
         <div className='shadow-2xl'>
             <GoogleLogin 
-                clientId=''
+                clientId={`${process.env.REACT_PUBLIC_GOOGLE_API_TOKEN}`}
                 render={(renderProps) => (
                 <button type='button'
                 className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
                 >
                     <FcGoogle className='mr-4'/>Sign in with Google
-
                 </button>
-            )}/>
+            )}
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy='single_host_origin'
+            />
         </div>
     </div>
   )
